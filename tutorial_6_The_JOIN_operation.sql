12f6e89a26a084936be355ab95acf4f7ca86044d
brugobi <brgobbi@hotmail.com>
2020-07-13 16:50:08 -0400

    add tutorial 7

--- tutorial_6_The_JOIN_operation.sql
+++ tutorial_6_The_JOIN_operation.sql
@@ -1,4 +1,4 @@
--- Tutorial 5 - The JOIN operation
+-- Tutorial 6 - The JOIN operation
 -- https://sqlzoo.net/wiki/The_JOIN_operation
 
 -- 1. The first example shows the goal scored by a player with the last name 'Bender'. The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime. Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
